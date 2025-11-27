7a5f63992b07f70d06ce2928644921339574c3b8
Jeff <woolridgejeff@proton.me>
2025-11-27 14:47:35 -0330

    fix

--- index.js
+++ index.js
@@ -205,6 +205,7 @@ app.post("/logout", (request, response) => {
 });
 
 // Start the server
+const PORT = 3000
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
     console.log(`Default users:
